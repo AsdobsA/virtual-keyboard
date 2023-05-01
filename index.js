582301a52e69f29ee111bb4957bfe38e1f4bc18f
Aliaksandr <alexsamakar@gmail.com>
2023-05-01 23:31:21 +0300

    feat: pressing a key

--- index.js
+++ index.js
@@ -32,9 +32,163 @@ const keyboard = [
     108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 32,
 ];
 
+const arr = [
+    "`",
+    `1`,
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "q",
+    "w",
+    "e",
+    "r",
+    "t",
+    "y",
+    "u",
+    "i",
+    "o",
+    "p",
+    "[",
+    "]",
+    "\\",
+    "a",
+    "s",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    ";",
+    "'",
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    ",",
+    ".",
+    "/",
+    ''
+];
+
+
+document.onkeydown = function (event) {
+    console.log(event)
+    for (let i = 0; i < containerKeyboard.childNodes.length; i++) {
+        if (event.key === containerKeyboard.childNodes[i].innerHTML) {
+            containerKeyboard.childNodes[i].classList.add("active");
+        }
+        if (event.code === 'ControlLeft') {
+            ctrlLeft.classList.add("active")
+        }
+        if (event.code === 'ControlRight') {
+            ctrlRigth.classList.add("active")
+        }
+        if (event.code === 'ShiftLeft') {
+            shiftLeft.classList.add("active")
+        }
+        if (event.code === 'ShiftRight') {
+            shiftRigth.classList.add("active")
+        }
+        if (event.code === 'AltLeft') {
+            altLeft.classList.add("active")
+        }
+        if (event.code === 'AltRight') {
+            altRight.classList.add("active")
+        }
+        if (event.code === 'MetaLeft') {
+            metaLeft.classList.add("active")
+        }
+        if (event.code === 'Delete') {
+            deleteButton.classList.add("active")
+        }
+        if (event.code === 'CapsLock') {
+            capsLock.classList.add("active")
+        }
+        if (event.code === 'ArrowLeft') {
+            arowLeft.classList.add("active")
+        }
+        if (event.code === 'ArrowRight') {
+            arowRigth.classList.add("active")
+        }
+        if (event.code === 'ArrowUp') {
+            arowUp.classList.add("active")
+        }
+        if (event.code === 'ArrowDown') {
+            arowDown.classList.add("active")
+        }
+        if (event.code === 'Space') {
+            space.classList.add("active")
+        }
+    }
+};
+
+document.onkeyup = function (event) {
+    console.log(event)
+    for (let i = 0; i < containerKeyboard.childNodes.length; i++) {
+        if (event.key === containerKeyboard.childNodes[i].innerHTML) {
+            containerKeyboard.childNodes[i].classList.remove("active");
+        }
+        if (event.code === 'ControlLeft') {
+            ctrlLeft.classList.remove("active")
+        }
+        if (event.code === 'ControlRight') {
+            ctrlRigth.classList.remove("active")
+        }
+        if (event.code === 'ShiftLeft') {
+            shiftLeft.classList.remove("active")
+        }
+        if (event.code === 'ShiftRight') {
+            shiftRigth.classList.remove("active")
+        }
+        if (event.code === 'AltLeft') {
+            altLeft.classList.remove("active")
+        }
+        if (event.code === 'AltRight') {
+            altRight.classList.remove("active")
+        }
+        if (event.code === 'MetaLeft') {
+            metaLeft.classList.remove("active")
+        }
+        if (event.code === 'Delete') {
+            deleteButton.classList.remove("active")
+        }
+        if (event.code === 'CapsLock') {
+            capsLock.classList.remove("active")
+        }
+        if (event.code === 'ArrowLeft') {
+            arowLeft.classList.remove("active")
+        }
+        if (event.code === 'ArrowRight') {
+            arowRigth.classList.remove("active")
+        }
+        if (event.code === 'ArrowUp') {
+            arowUp.classList.remove("active")
+        }
+        if (event.code === 'ArrowDown') {
+            arowDown.classList.remove("active")
+        }
+        if (event.code === 'Space') {
+            space.classList.remove("active")
+        }
+    }
+};
+
 function init() {
     let out = "";
-    for (let i = 0; i < keyboard.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         if (i === 13) {
             out +=
                 '<div class="backspace">Backspace</div>' +
@@ -48,30 +202,42 @@ function init() {
         if (i === 37) {
             out +=
                 '<div class="enter">Enter</div>' +
-                '<div class="shift-left">Shift</div>';
+                '<div class="shift-left">Shift.</div>';
         }
         if (i === 47) {
             out +=
                 '<div class="arrow-up">&#9650;</div>' +
-                '<div class="shift-rigth">Shift</div>' +
+                '<div class="shift-rigth">Shift.</div>' +
                 '<div class="ctrl-left">Ctrl</div>' +
                 '<div class="win">Win</div>' +
-                '<div class="alt-left">Alt</div>' +
+                '<div class="alt-left">Alt.</div>' +
                 '<div class="space"></div>' +
-                '<div class="alt-rigth">Alt</div>' +
+                '<div class="alt-rigth">Alt.</div>' +
                 '<div class="arrow-left">&#9668;</div>' +
                 '<div class="arrow-down">&#9660;</div>' +
                 '<div class="arrow-rigth">&#9658;</div>' +
                 '<div class="ctrl-rigth">Ctrl</div>';
         }
-        out +=
-            '<div class="key-keyboard">' +
-            String.fromCharCode(keyboard[i]) +
-            "</div>";
+        out += '<div class="key-keyboard">' + arr[i] + "</div>";
     }
     containerKeyboard.innerHTML = out;
 }
 init();
+
+const ctrlLeft = document.querySelector('.ctrl-left');
+const ctrlRigth = document.querySelector('.ctrl-rigth');
+const shiftLeft = document.querySelector('.shift-left');
+const shiftRigth = document.querySelector('.shift-rigth');
+const altLeft = document.querySelector('.alt-left');
+const altRight = document.querySelector('.alt-rigth');
+const metaLeft = document.querySelector('.win');
+const deleteButton = document.querySelector('.del');
+const capsLock = document.querySelector('.caps-lock');
+const arowLeft = document.querySelector('.arrow-left');
+const arowRigth = document.querySelector('.arrow-rigth');
+const arowUp = document.querySelector('.arrow-up');
+const arowDown = document.querySelector('.arrow-down');
+const space = document.querySelector('.space');
 
 function writeButton() {
     const buttonKeybord = document.querySelectorAll(".key-keyboard");
